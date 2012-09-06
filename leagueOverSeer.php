5bcfb140801d15e2c8c11a374bfa11a8df4e1a76
Vladimir Jimenez <allejo@me.com>
2012-09-05 17:37:26 -0700

    Added report match capability

--- leagueOverSeer.php
+++ leagueOverSeer.php
@@ -63,7 +63,7 @@ if (isset($_GET['league']))
                 $player = $_GET['player']; //Get the player name
                 $player = mysqli_real_escape_string($dbc, $player); //Clean it up from MySQL injection
                 $getTeam = "SELECT `teamid` FROM `players` WHERE name = '" . $player . "'"; //Get the teamid of the team the players belongs to
-                $result = mysqli_query($dbc, $getTeam); //Execute the query
+                $result = @mysqli_query($dbc, $getTeam); //Execute the query
                 
                 if (mysqli_num_rows($result) == 0)
                 {
@@ -80,7 +80,7 @@ if (isset($_GET['league']))
                     }
                     
                     $getTeamName = "SELECT `name` FROM `teams` WHERE `id` = " . $playerID[0] . " LIMIT 1"; //Get the name of the team with the teamid that we got before
-                    $teamName = mysqli_query($dbc, $getTeamName);
+                    $teamName = @mysqli_query($dbc, $getTeamName);
                     $playerTeamName = mysqli_fetch_array($teamName);
                     
                     echo $playerTeamName[0]; //Print it out
@@ -90,38 +90,96 @@ if (isset($_GET['league']))
             
             case 'reportMatch':
             {
-                require_once(dirname(dirname(__FILE__)) . '/Matches/match.php');
-                //enter_match($team_id1, $team_id2, $team1_caps, $team2_caps, $timestamp, $duration)
+                $site = "null"; //Because I need to be able to include the match.php file, $site has to be set
+                $redTeamWins = $_GET['redTeamWins'];
+                $redTeamWins = mysqli_real_escape_string($dbc, $redTeamWins);
+                $purpleTeamWins = $_GET['purpleTeamWins'];
+                $purpleTeamWins = mysqli_real_escape_string($dbc, $purpleTeamWins);
+                $timestamp = $_GET['matchTime'];
+                $timestamp = mysqli_real_escape_string($dbc, $timestamp);
+                $duration = $_GET['duration'];
+                $duration = mysqli_real_escape_string($dbc, $duration);
                 
-                $redPlayers = explode('"', $_GET['redPlayers']);
-                $getRedTeam = "SELECT `teamid` FROM `players` WHERE";
-                foreach ($redPlayers as $redPlayer)
+                //Let's get all the red players' callsigns
+                $redPlayers = explode('"', $_GET['redPlayers']); //Get individual callsigns
+                $redPlayerCount = count ($redPlayers); //Get the number of players
+                $counter = 0; //We're keeping count
+                
+                $getRedTeam = "SELECT `teamid` FROM `players` WHERE `name` IN("; //Start the MySQL query
+                foreach ($redPlayers as $rp) //Add all the player's callsigns to the MySQL query
                 {
-                    $redPlayer = mysql_real_escape_string($redPlayer);
-                    $getRedTeam .= " `name` = " . $redPlayer;
+                    $counter++;
+                    $rp = mysqli_real_escape_string($dbc, $rp);
+                    $getRedTeam .= "'" . $rp . "'";
+                    
+                    if ($counter + 1 === $redPlayerCount) //Only add an comma in there is still another element in the array
+                        $getRedTeam .= ", ";
+                    if ($counter === $redPlayerCount) //Only add a ')' if it's the last item
+                        $getRedTeam .= ")";
+                }
+                $redTeamResult = @mysqli_query($dbc, $getRedTeam);
+                
+                //Let's get all the purple players' callsign, do the same things as with the red team
+                $purplePlayers = explode('"', $_GET['purplePlayers']);
+                $purplePlayerCount = count($purplePlayers);
+                $counter = 0;
+                $getPurpleTeam = "SELECT `teamid` FROM `players` WHERE `name` IN(";
+                foreach ($purplePlayers as $pp)
+                {
+                    $counter++;
+                    $pp = mysqli_real_escape_string($dbc, $pp);
+                    $getPurpleTeam .= "'" . $pp . "'";
+                    
+                    if ($counter + 1 === $purplePlayerCount)
+                        $getPurpleTeam .= ", ";
+                    if ($counter === $purplePlayerCount)
+                        $getPurpleTeam .= ")";
+                }
+                $purpleTeamResult = @mysqli_query($dbc, $getPurpleTeam);
+                
+                $redTeamIDs = mysqli_fetch_array($redTeamResult);
+                $purpleTeamIDs = mysqli_fetch_array($purpleTeamResult);
+                
+                if (mysqli_num_rows($redTeamResult) == 0 || mysqli_num_rows($purpleTeamResult) == 0) //A player that didn't belong to the team ruined the match
+                {
+                    echo "Teams could not be reported properly. Please message a referee with the match data.";
+                    die();
                 }
                 
-                $redTeamResult = mysql_query($getRedTeam);
+                //Get a player's team id
+                $redTeamID = $redTeamIDs[0];
+                $purpleTeamID = $purpleTeamIDs[0];
                 
-                $greenPlayers = explode('"', $_GET['redPlayers']);
-                $getGreenTeam = "SELECT `teamid` FROM `players` WHERE";
-                foreach ($greenPlayers as $greenPlayer)
-                {
-                    $greenPlayer = mysql_real_escape_string($greenPlayer);
-                    $getGreenTeam .= " `name` = " . $greenPlayer;
-                }
-                
-                $greenTeamResult = mysql_query($getGreenTeam);
-                
-                $redTeamIDs = mysql_fetch_array($redTeamResult);
-                $greenTeamIDs = mysql_fetch_array($greenTeamResult);
-                
+                /*
+                Go through all the IDs to make sure everyone is on the same
+                team. If someone isn't part of the same team, don't report
+                the match as it was invalid.
+                */
                 foreach ($redTeamIDs as $rID)
                 {
-                
+                    if ($rID != $redTeamID)
+                    {
+                        echo "There was an invalid team member on the red team. Please report the match to a referee.";
+                        die();
+                    }
+                }
+                foreach ($purpleTeamIDs as $pID)
+                {
+                    if ($pID != $purpleTeamID)
+                    {
+                        echo "There was an invalid team member on the purple team. Please report the match to a referee.";
+                        die();
+                    }
                 }
                 
-                $where = '';
+                echo "no errors.. about to report...";
+                
+                require("./Matches/match.php");
+                //ob_start();
+                /*$tmp =*/ enter_match($redTeamID, $purpleTeamID, $redTeamWins, $purpleTeamWins, $timestamp, $duration);
+                //ob_end_clean();
+                
+                echo "No errors entirely!!";
             }
             break;
             

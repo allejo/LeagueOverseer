4ea2e5d7a634785c0e47825080dd8c812c8a5ea9
allejo <allejo@me.com>
2013-03-31 14:09:58 -0700

    Converted  to

--- leagueOverSeer.cpp
+++ leagueOverSeer.cpp
@@ -34,13 +34,13 @@ League Over Seer Plug-in
 const int MAJOR = 0;
 const int MINOR = 9;
 const int REV = 9;
-const int BUILD = 111;
+const int BUILD = 112;
 
 class leagueOverSeer : public bz_Plugin, public bz_CustomSlashCommandHandler, public bz_BaseURLHandler
 {
     sqlite3* db; //sqlite database we'll be using
 
-    virtual const char* Name (){return "League Over Seer 0.9.9 r111";}
+    virtual const char* Name (){return "League Over Seer 0.9.9 r112";}
     virtual void Init ( const char* config);
     virtual void Event( bz_EventData *eventData );
     virtual bool SlashCommand( int playerID, bz_ApiString, bz_ApiString, bz_APIStringList*);

b3c0da7786d4b85e5362a3693e47faf14be9d305
allejo <allejo@me.com>
2013-05-25 16:56:30 -0700

    Fixed revision count for plugin

--- leagueOverSeer.cpp
+++ leagueOverSeer.cpp
@@ -34,13 +34,13 @@ League Over Seer Plug-in
 const int MAJOR = 0;
 const int MINOR = 9;
 const int REV = 9;
-const int BUILD = 117;
+const int BUILD = 120;
 
 class leagueOverSeer : public bz_Plugin, public bz_CustomSlashCommandHandler, public bz_BaseURLHandler
 {
     sqlite3* db; //sqlite database we'll be using
 
-    virtual const char* Name (){return "League Over Seer 0.9.9 r117";}
+    virtual const char* Name (){return "League Over Seer 0.9.9 r120";}
     virtual void Init ( const char* config);
     virtual void Event( bz_EventData *eventData );
     virtual bool SlashCommand( int playerID, bz_ApiString, bz_ApiString, bz_APIStringList*);

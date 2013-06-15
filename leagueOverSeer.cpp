17afeff6a41922c10d4e26ec300c5345beee08a1
allejo <allejo@me.com>
2013-06-15 12:11:49 -0700

    Last version bump... Typo in README

--- leagueOverSeer.cpp
+++ leagueOverSeer.cpp
@@ -35,13 +35,13 @@ League Over Seer Plug-in
 const int MAJOR = 0;
 const int MINOR = 9;
 const int REV = 9;
-const int BUILD = 131;
+const int BUILD = 132;
 
 class leagueOverSeer : public bz_Plugin, public bz_CustomSlashCommandHandler, public bz_BaseURLHandler
 {
     sqlite3* db; //sqlite database we'll be using
 
-    virtual const char* Name (){return "League Over Seer 0.9.9 r131";}
+    virtual const char* Name (){return "League Over Seer 0.9.9 r132";}
     virtual void Init ( const char* config);
     virtual void Event( bz_EventData *eventData );
     virtual bool SlashCommand( int playerID, bz_ApiString, bz_ApiString, bz_APIStringList*);

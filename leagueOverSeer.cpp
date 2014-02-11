1df7ca5f7abb879ca9431fd8ca8988be5c673f54
allejo <allejo@me.com>
2014-02-11 00:44:10 -0800

    Version bump

--- leagueOverSeer.cpp
+++ leagueOverSeer.cpp
@@ -38,7 +38,7 @@ League Overseer
 const int MAJOR = 1;
 const int MINOR = 1;
 const int REV = 0;
-const int BUILD = 258;
+const int BUILD = 260;
 
 // The API number used to notify the PHP counterpart about how to handle the data
 const int API_VERSION = 1;
